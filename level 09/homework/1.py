11abc2db503d0718a212d04e1cf0c8e03f2e93d6
andria2527 <andriamach2@gmail.com>
2026-07-05 09:43:11 +0400

    second commit

---
+++ level 09/homework/1.py
@@ -0,0 +1,13 @@
+for i in range(1,101):
+    print("andria")
+
+num = 0
+for i in range(9, 99):
+    num = num + i
+    print(num)
+
+for i in range(1, 1000, 2):
+    print(i)
+
+for i in range(4, 500, 2):
+    print(i)
