343eebd0411313f367d75c8686ac79e77de39752
andria2527 <andriamach2@gmail.com>
2026-07-28 13:22:46 +0400

    second commit

---
+++ level 13/classwordk/1.py
@@ -0,0 +1,24 @@
+# num1 = int(input("shemoiyvane ricxvi: "))
+# if num1 >= 18:
+#     print("dadebitia")
+# else:
+#     print("uaryofitia an nulia")
+#-------------------------------    ----------------------------------  ----------------------------                                            --
+# age = int(input("shemoiyvane sheni asaki: "))
+# if age >= 18:
+#     print("srulwlovania")
+# else:
+#     print("arasrulwlovania")
+#--------------------------------------------------------------             --------------------------------
+# age = int(input("shemoiyvane sheni qula: "))
+# if age >= 50:
+#     print("chaabara")
+# else:
+#     print("ar chaabara")
+#-----------------------------------------------------------                    =---------------------------
+password1 = input("shemoiyvane sheni paroli: ")
+password = "python123"
+if password == password1:
+    print("sworia")
+else:
+    print("arasworia")
