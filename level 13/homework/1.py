85508d09eb26ad2bfb3825c1dabcd9f036b34308
andria2527 <andriamach2@gmail.com>
2026-08-09 19:51:01 +0400

    second commit

---
+++ level 13/homework/1.py
@@ -0,0 +1,37 @@
+#if-else statement vityenebtimashi rom tu magalitad ifi ar gaamartlebs else-i aucileblad gaamartlebs
+# num = int(input("shemoiyvane raime ricxvi: "))
+# num1 = int(input("shemoiyvane meore raime ricxvi: "))
+# if num >= num1:
+#     print("პირველი მეტია")
+# else:
+#     print("მეორე მეტია ან ტოლია")
+# num = int(input("shemoiyvane raime ricxvi: "))
+# if num <= 100:
+#     print("100-ზე მეტია")
+# else:
+#     print("100-ზე ნაკლები ან ტოლია")
+# name = str(input("shemoiyvane sheni saxeli: "))
+# if name == "Nika":
+#     print("გამარჯობა, ნიკა!")
+# else:
+#     print("უცნობი მომხმარებელი")
+# temp = int(input("shemoiyvane sheni temperatura: "))
+# if temp < 0:
+#     print("iyineba")
+# else:
+#     print("ar iyineba")
+# fasi = int(input("shemoiyvane fasi: "))
+# if fasi < 100:
+#     print("100-ze naklebia fasi")
+# else:
+#     print("100-ze metia fasi")
+# num = int(input("shemoiyvane raime ricxvi: "))
+# if num == 0:
+#     print("nulia")
+# else:
+#     print("nulze metia an naklebia")
+num = int(input("shemoiyvane sheni angarishi: "))
+if num >= 50:
+    print("sakmarisi balansi")
+else:
+    print("balansi arasakmarisia")
