a5fac79e553423ea820b837c23893d46b554010b
andria2527 <andriamach2@gmail.com>
2026-09-12 11:30:53 +0400

    second commit

---
+++ level 19/classwork/1.py
@@ -0,0 +1,13 @@
+# list1 = [1,2,3,4,5,6,7,8,9,10]
+# print(list1[-5:-2])
+
+# string = "andria"
+# print(string[2:])
+
+# string1 = "andria"
+# list1 = ["andria", "giorgi", "shio", "niko"]
+# print(string1[::-1])
+# print(list1[::-1])
+
+# list = ["apple", 67, 20.5, "kiwi", 61, 68.9]
+# print(list[2::-3])
---
+++ level 19/homework/1.py
@@ -0,0 +1,32 @@
+#indexing bolodan aris ukugma amogeba listebis anu es aris
+#list = ["banana", "kiwi", "apple"]
+#            -3      -2      -1
+# numbers = [10, 20, 30, 40, 50, 60, 70, 80]
+# print(numbers[0:4])
+# print(numbers[5:])
+# print(numbers[1:5])
+# print(numbers[::-1])
+# word = "PYTHONPROGRAMMING"
+# print(word[0:6])
+# print(word[6:17])
+# print(word[:5])
+# print(word[12:17])
+# print(word[::-1])
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+# print(letters[-1:])
+# print(letters[-4:])
+# print(letters[:-2])
+# print(letters[-5:-2])
+# print(letters[::-1])
+numbers = [1, 2, 3, 4, 5, 6]
+# numbers[2] = 30
+# numbers[3] = 40
+# print(numbers)
+numbers[0] = 10
+numbers[1] = 20
+numbers[2] = 30
+print(numbers)
+numbers = [1,2,3,4,5,6,7,8]
+
+
+
